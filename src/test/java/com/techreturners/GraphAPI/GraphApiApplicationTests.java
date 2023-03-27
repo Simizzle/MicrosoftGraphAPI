35b760892b07860db789a6f806c9dc219d2eaa39
Simon Morgan <simonmorgan@Admins-MacBook-Pro-3.local>
2023-03-27 20:40:26 +0100

    Initial commit

---
+++ src/test/java/com/techreturners/GraphAPI/GraphApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techreturners.GraphAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
